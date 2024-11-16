710e6b19175f36318393d98c83e1384522693cc3
Данило Кучерук < @DESKTOP-L6AD50V>
2024-11-16 13:07:12 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть ціле число:");
+        if (int.TryParse(Console.ReadLine(), out int number))
+        {
+            Console.WriteLine($"Ви ввели число {number}");
+        }
+        else
+        {
+            Console.WriteLine("Це не є цілим числом.");
+        }
+    }
+}
